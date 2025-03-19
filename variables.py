591a3f539958dd62ad5ab6d77783db4cce3e485a
AveBan <esteban.acevedososa@gmail.com>
2025-03-19 16:52:38 -0300

    añadiendo variables y tipos de condiciones

---
+++ variables.py
@@ -0,0 +1,77 @@
+# #nombre = dato
+# my_variable = 10
+
+# #Tipos de datos simples
+
+# #variable numerica
+# numero = 10
+
+# #variable "String"
+
+# my_text = "el resultado de la suma es "
+
+# #variable booleana
+
+# my_bool = True 
+# my_bool2 = False
+
+# #variable float
+
+# myFloat = 20.1
+
+#          #Operadores
+         
+# resultado = my_variable + myFloat
+# print(my_text , resultado)
+
+# print(f"el resultado es el siguiente: {resultado}")
+
+# numero1 = 50
+# numero2 = 100
+
+# #Suma
+# r_suma = numero1 + numero2
+# print(f"El resultado de la suma es: {r_suma}")
+
+# #Resta
+# r_resta = numero2 - numero1
+# print(f"El resultado de la resta es: {r_resta}")
+
+# #Multipicacion
+# r_multiplicacion = numero1 * numero2
+# print(f"El resultado de la multiplicacion es: {r_multiplicacion}")
+
+# #Division
+# r_division = numero2 / numero1
+# print(f"El resultado de la division es: {r_division}")
+
+# #DivisionEntera
+# r_divisione = numero2 // numero1
+# print(f"El resultado de la division es: {r_divisione}")
+
+# #Potencia
+# r_potencia = numero1 ** numero2
+# print(f"El resultado de potencia es: {r_potencia}")
+
+# #CONDICIONALES
+# #IF, ELSE, ELIF
+
+# Mayor que = >
+# Menor que = <
+# Mayor o igual que =>
+# Menor o igual que =<
+# Distinto que = !=
+
+
+edad = int(input("Dime tu edad: "))
+mayoria_edad = 18
+
+if edad < mayoria_edad:
+    print("No puede porque eres menor de edad. ")
+elif edad < 16:
+    print("Ni siquiera tienes 16")
+else:
+    print("toma compra un caramelo")
+    
+
+
